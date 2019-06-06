cfbd8c3e889b98bf7b91313f6ed71e7789cc84ac
MaxwellCoelho <maxwellfcoelho@gmail.com>
2019-06-06 18:54:30 +0000

    Update MaxProductOfThree.js

--- Lesson-6-Sorting/MaxProductOfThree.js
+++ Lesson-6-Sorting/MaxProductOfThree.js
@@ -2,22 +2,46 @@
 
 Task description:
 
-Write a function
+A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
+
+For example, array A such that:
+
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+  
+contains the following example triplets:
+
+    (0, 1, 2), product is −3 * 1 * 2 = −6
+    (1, 2, 4), product is 1 * 2 * 5 = 10
+    (2, 4, 5), product is 2 * 5 * 6 = 60
+
+Your goal is to find the maximal product of any triplet.
+
+Write a function:
 
     function solution(A);
 
-that, given an array A consisting of N integers, returns the number of distinct values in array A.
+that, given a non-empty array A, returns the value of the maximal product of any triplet.
 
-For example, given array A consisting of six elements such that:
+For example, given array A such that:
 
- A[0] = 2    A[1] = 1    A[2] = 1
- A[3] = 2    A[4] = 3    A[5] = 1
-the function should return 3, because there are 3 distinct values appearing in array A, namely 1, 2 and 3.
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+  
+the function should return 60, as the product of triplet (2, 4, 5) is maximal.
 
 Write an efficient algorithm for the following assumptions:
 
-    - N is an integer within the range [0..100,000];
-    - each element of array A is an integer within the range [−1,000,000..1,000,000].
+    - N is an integer within the range [3..100,000];
+    - each element of array A is an integer within the range [−1,000..1,000].
 
 Solution:
 */
