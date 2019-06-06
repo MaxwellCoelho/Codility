91f276a86b8ec60982106d7493d3163f35d91c07
MaxwellCoelho <maxwellfcoelho@gmail.com>
2019-06-06 19:14:32 +0000

    Create Brackets.js

---
+++ Lesson-7-Stacks-And-Queues/Brackets.js
@@ -0,0 +1,43 @@
+/*
+Task description:
+
+A string S consisting of N characters is considered to be properly nested if any of the following conditions is true:
+
+  S is empty;
+  S has the form "(U)" or "[U]" or "{U}" where U is a properly nested string;
+  S has the form "VW" where V and W are properly nested strings.
+  
+For example, the string "{[()()]}" is properly nested but "([)()]" is not.
+
+Write a function:
+
+  function solution(S);
+
+that, given a string S consisting of N characters, returns 1 if S is properly nested and 0 otherwise.
+
+For example, given S = "{[()()]}", the function should return 1 and given S = "([)()]", the function should return 0, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+  - N is an integer within the range [0..200,000];
+  - string S consists only of the following characters: "(", "{", "[", "]", "}" and/or ")".
+
+Solution:
+*/
+
+function solution(S) {
+    S = S.split('');
+                
+    let obj = ['()', '[]', '{}'],
+        stack = [];
+
+    for (let n = 0; n < S.length; n++) {
+        if (stack.length > 0 && obj.indexOf(stack[stack.length - 1] + S[n]) != -1) {
+            stack.pop();
+        } else {
+            stack.push(S[n]);
+        }
+    }
+
+    return stack.length == 0 ? 1 : 0;
+}
